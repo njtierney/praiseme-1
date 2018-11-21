b0c18b76d02c700380e2930283b770f0fe7ddd75
Saskia Freytag <freytag.s@wehi.edu.au>
2018-11-21 16:16:22 +1100

    changed to a glue function

--- R/praise.R
+++ R/praise.R
@@ -1,17 +1,18 @@
 #' @title Praise someone
 #' @description Let's dole out praise
 #' @param name Specifies recipient of praise
-#' @examples praise("Robbie")
-#' @examples praise("Saskia")
+#' @examples
+#' praise("Saskia")
+#' praise("Robbie")
 #' @export
 
 
 praise <- function(name="Saskia"){
 
   if(name=="Robbie"| name=="Roxy")  {
-    return(paste0(name, ", you are NOT r-some!"))
+    return(glue::glue("{name}, you are NOT r-some!"))
   } else{
-    return(paste0(name, ", you are r-some!"))
+    return(glue::glue("{name}, you are r-some!"))
   }
 
 }
